0ee457aa78c9d7a905a65b77da3847f641a7476f
Martin Smetanka <martin.smetanka@gmail.com>
2022-02-28 02:30:08 +0100

    feat: simple barrier example added

---
+++ simpleBarrier.py
@@ -0,0 +1,72 @@
+"""Authors: Mgr. Ing. Matúš Jókay, PhD.
+            Bc. Martin Smetanka
+University:  Slovak Technical University in Bratislava
+Faculty: Faculty of Electrical Engineering and Information Technology
+Semester: Spring/Summer
+Year: 2022
+License: MIT
+Assignment: https://uim.fei.stuba.sk/i-ppds/2-cvicenie-turniket-bariera-%f0%9f%9a%a7/?%2F"""
+
+from random import randint
+from time import sleep
+from fei.ppds import Thread
+from fei.ppds import Semaphore
+from fei.ppds import Mutex
+from fei.ppds import Event
+from fei.ppds import print
+from threading import get_ident
+
+
+class SimpleBarrier:
+    def __init__(self, n_threads):
+        self.threads = n_threads
+        self.counter = 0
+        self.mutex = Mutex()
+        self.semaphore = Event()
+
+    def wait(self):
+        self.mutex.lock()
+        self.counter += 1
+        if self.counter == self.threads:
+            self.counter = 0
+            self.semaphore.signal()
+        self.mutex.unlock()
+        self.semaphore.clear()
+        self.semaphore.wait()
+        self.semaphore.signal()
+
+
+def barrier_example(barrier, thread_id):
+    """Predpokladajme, ze nas program vytvara a spusta 5 vlakien,
+    ktore vykonavaju nasledovnu funkciu, ktorej argumentom je
+    zdielany objekt jednoduchej bariery
+    """
+    sleep(randint(1, 10) / 10)
+    print("vlakno %d pred barierou" % thread_id)
+    barrier.wait()
+    print("vlakno %d po bariere" % thread_id)
+
+
+def before_barrier(thread_id):
+    sleep(randint(1, 10) / 10)
+    print(f"pred barierou {thread_id}")
+
+
+def after_barrier(thread_id):
+    print(f"za barierou {thread_id}")
+    sleep(randint(1, 10) / 10)
+
+
+def barrier_cycle(barrier_1, barrier_2, thread_id):
+    while True:
+        before_barrier(thread_id)
+        barrier_1.wait()
+        after_barrier(thread_id)
+        barrier_2.wait()
+
+
+THREADS = 10
+sb_1 = SimpleBarrier(THREADS)
+sb_2 = SimpleBarrier(THREADS)
+threads = [Thread(barrier_cycle, sb_1, sb_2, i) for i in range(THREADS)]
+[t.join() for t in threads]
