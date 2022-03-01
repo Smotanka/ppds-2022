4683e23b012925e8ffea65eed5b41b5a5acf982e
Martin Smetanka <martin.smetanka@gmail.com>
2022-03-01 01:01:43 +0100

    style: added method and function labels to barrierCycle and simpleBarrier

--- barrierCycle.py
+++ barrierCycle.py
@@ -7,47 +7,93 @@ Year: 2022
 License: MIT
 Assignment: https://uim.fei.stuba.sk/i-ppds/2-cvicenie-turniket-bariera-%f0%9f%9a%a7/?%2F"""
 
+# Python v3.10
 from random import randint
 from time import sleep
 from fei.ppds import Thread
-from fei.ppds import Semaphore
 from fei.ppds import Mutex
 from fei.ppds import Event
 from fei.ppds import print
-from threading import get_ident
 
 
 class SimpleBarrier:
+    """
+
+    Object that represents barrier synchronization method.
+
+    """
     def __init__(self, n_threads):
+        """"
+        Creates instance of barrier.
+
+            @param(n_threads): int - number of threads
+
+            @attrib(threads): int - number of threads, with which this instance of barrier object will be working
+            @attrib(counter): int - simple counter, that holds number of threads
+            @attrib(mutex): Object - 'Mutex' lock object
+            @attrib(event): Object - 'Event' object that locks multiple threads
+
+        """
         self.threads = n_threads
         self.counter = 0
         self.mutex = Mutex()
-        self.semaphore = Event()
+        self.event = Event()
 
     def wait(self):
-        self.semaphore.clear()
+        """
+
+        Method that implements barrier synchronization, 'wait' method waits for execution on all threads,
+        this method is created to work in loop, where last thread unlocks threads from previous iteration
+
+        """
+        self.event.clear()
         self.mutex.lock()
         self.counter += 1
         if self.counter == self.threads:
             self.counter = 0
-            self.semaphore.signal()
+            self.event.signal()
         self.mutex.unlock()
         sleep(randint(1, 10) / 10)
         if self.counter < self.threads:
-            self.semaphore.wait()
+            self.event.wait()
 
 
 def before_barrier(thread_id):
-    sleep(randint(1, 10) / 10)
-    print(f"pred barierou {thread_id}")
+    """
+
+    Function that prints all threads that are waiting on turn-stile to open.
+
+        @param(thread_id): int - id that represent current working thread
+
+    """
+
+    sleep(randint(1, 10) / 10)  # Illustrates code execution
+    print(f"before barrier {thread_id}")
 
 
 def after_barrier(thread_id):
-    print(f"za barierou {thread_id}")
-    sleep(randint(1, 10) / 10)
+    """
+
+    Function that prints all threads that are freed in turn-stile.
+
+        @param(thread_id): int - id that represent current working thread
+
+    """
+
+    print(f"after barrier {thread_id}")
+    sleep(randint(1, 10) / 10)  # Illustrates code execution
 
 
 def barrier_cycle(barrier_1, barrier_2, thread_id):
+    """
+
+    Function recreating example how to use ATD barrier
+
+        @param(barrier_1): Object - reference to an 'SimpleBarrier' object
+        @param(barrier_2): Object - reference to an 'SimpleBarrier' object
+        @param(thread_id): int - id that represent current working thread
+
+    """
     while True:
         before_barrier(thread_id)
         barrier_1.wait()
@@ -58,5 +104,6 @@ def barrier_cycle(barrier_1, barrier_2, thread_id):
 THREADS = 5
 sb_1 = SimpleBarrier(THREADS)
 sb_2 = SimpleBarrier(THREADS)
+# creates array of size 'THREADS' and on each thread is called 'barrier_cycle' function
 threads = [Thread(barrier_cycle, sb_1, sb_2, i) for i in range(THREADS)]
 [t.join() for t in threads]
--- simpleBarrier.py
+++ simpleBarrier.py
@@ -7,23 +7,48 @@ Year: 2022
 License: MIT
 Assignment: https://uim.fei.stuba.sk/i-ppds/2-cvicenie-turniket-bariera-%f0%9f%9a%a7/?%2F"""
 
+# Python v3.10
 from random import randint
 from time import sleep
 from fei.ppds import Thread
 from fei.ppds import Semaphore
 from fei.ppds import Mutex
 from fei.ppds import print
-from threading import get_ident
 
 
 class SimpleBarrier:
+    """
+
+    Object that represents barrier synchronization method.
+
+    """
+
     def __init__(self, n_threads):
+        """"
+        Creates instance of barrier.
+
+            @param(n_threads): int - number of threads
+
+            @attrib(threads): int - number of threads, with which this instance of barrier object will be working
+            @attrib(counter): int - simple counter, that holds number of threads
+            @attrib(mutex): Object - 'Mutex' lock object
+            @attrib(semaphore): Object - 'Semaphore' object that implements 'random', 'lifo', 'fifo queue
+
+        """
+
         self.threads = n_threads
         self.counter = 0
         self.mutex = Mutex()
         self.semaphore = Semaphore(0)
 
     def wait(self):
+        """
+
+        Method that implements barrier synchronization, 'wait' method waits for execution on all threads,
+        then method reset counter and free all threads.
+
+        """
+
         self.mutex.lock()
         self.counter += 1
         if self.counter == self.threads:
@@ -34,6 +59,15 @@ class SimpleBarrier:
 
 
 def barrier_example(barrier, thread_id):
+    """
+
+    Function recreating example how to use barrier
+
+        @param(barrier): Object - reference to an 'SimpleBarrier' object
+        @param(thread_id): int - id that represent current working thread
+
+    """
+
     sleep(randint(1, 10) / 10)
     print("thread %d before barrier" % thread_id)
     barrier.wait()
@@ -42,5 +76,6 @@ def barrier_example(barrier, thread_id):
 
 THREADS = 5
 sb_1 = SimpleBarrier(THREADS)
+# creates array of size 'THREADS' and on each thread is called 'barrier_example' function
 threads = [Thread(barrier_example, sb_1, i) for i in range(THREADS)]
 [t.join() for t in threads]
