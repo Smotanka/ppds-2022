e10061920092840b95bfc7b04f604a48358e7975
Martin Smetanka <martin.smetanka@gmail.com>
2022-04-11 21:17:22 +0200

    feat: minor changes

--- scheduler.py
+++ scheduler.py
@@ -3,10 +3,10 @@ from queue import Queue
 
 class Task(object):
 
-    def __init__(self, fun):
+    def __init__(self, fun, *args):
         self.id = None
         self.fun = fun
-        self.args = None
+        self.args = args
 
     def setId(self, task_id):
         self.id = task_id
@@ -14,8 +14,11 @@ class Task(object):
     def getId(self):
         return self.id
 
-    def run(self):
-        return self.fun.send(self.args)
+    def execute(self):
+        try:
+            return self.fun.send(self.args)
+        except TypeError:
+            return self.fun.send(None)
 
 
 class Scheduler(object):
@@ -31,33 +34,41 @@ class Scheduler(object):
         while True:
             try:
                 task = self.q.get()
-                task.run()
+                task.execute()
                 self.q.task_done()
                 print(f"task {task.getId()} is done")
                 self.schedule(task)
             except StopIteration:
+                print(f"task {task.getId()} is deleted")
+                del task
                 continue
 
     def schedule(self, task):
         self.q.put(task)
 
     def new(self, fun):
-        new = Task(fun)
+        new = Task(fun, None)
         new.setId(self._generateTaskId())
         self.schedule(new)
 
 
 def foo(str1, str2):
+    str1 = str1 + str2
     print(str1)
-    yield
-    print(str2)
-    yield
+    yield str1
+    str1 = str1 + bar(str1)
+    print(str1)
+    yield str1
+
+
+def bar(str1):
+    return str1 + 'cd'
 
 
 def main():
     sched = Scheduler()
-    sched.new(foo('1', '2'))
-    sched.new(foo('3', '4'))
+    sched.new(foo('a', 'b'))
+    sched.new(foo('a', 'b'))
     sched.main()
 
 
