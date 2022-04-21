4bad0403e5fd91066e1534c1016c565ab43dfeab
Martin Smetanka <martin.smetanka@gmail.com>
2022-04-21 20:39:04 +0200

    style: added comments and labels

--- appSync.py
+++ appSync.py
@@ -1,31 +1,66 @@
+"""Authors: Bc. Martin Smetanka
+University:  Slovak Technical University in Bratislava
+Faculty: Faculty of Electrical Engineering and Information Technology
+Semester: Spring/Summer
+Year: 2022
+License: MIT
+Assignment: https://uim.fei.stuba.sk/i-ppds/8-cvicenie-asynchronne-programovanie/"""
+
 import base64
-from http.server import BaseHTTPRequestHandler, HTTPServer
 import requests
 import time
-from utils.utils import  save_time
+from http.server import BaseHTTPRequestHandler
+from http.server import HTTPServer
+from utils.utils import save_time
 
 
-class MyServer(BaseHTTPRequestHandler):
+# Python v3.10
+
+class MySyncServer(BaseHTTPRequestHandler):
+    """
+    Class representing http server with asynchronous requesting for images.
+    """
+
     def do_GET(self):
+        """
+        Methods that returns content when user sends GET request to a server, in our case
+        html page with random images (number of images is parameter passed to 'get_images' function)
+        :return: None
+        """
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
-        start = time.time()
+
+        start = time.time()  # start counting how long to get '15' images
         images = get_images('https://picsum.photos/200/300', 15)
         end = time.time()
         elapsed = end - start
+
         save_time('utils/sync_elapsed.txt', "{:.4f}".format(elapsed))
+
+        # create HTML page
         INDEX = """<html><head><title>Title</title></head><body><p>time elapsed: """ + "{:.4f}".format(elapsed) + \
                 """</p>""" + create_images(images) + """</body></html> """
         self.wfile.write(bytes(INDEX, 'utf-8'))
 
 
 def get_image(url: str) -> str:
+    """
+    Create request to an 'url' and returns content in base64 encoding
+    :param url: string: - url for request
+    :return: string: - base64 string
+    """
     img = base64.b64encode(requests.get(url=url).content).decode('UTF-8')
     return img
 
 
 def get_images(url: str, num: int) -> list[str]:
+    """
+    Synchronously create request to a given url and returns content of the requests
+    :param url: string: - url for request
+    :param num: int: - number of request
+    :return: Request
+    """
     result = []
     for _ in range(num):
         result.append(get_image(url))
@@ -33,6 +68,11 @@ def get_images(url: str, num: int) -> list[str]:
 
 
 def create_images(arr: list) -> str:
+    """
+    Create html div with images stored in 'arr'
+    :param arr: list: - list with base64 coded images
+    :return: str: - html div element
+    """
     out = """<div>"""
     for img in arr:
         out += """<img src=data:image/png;base64,""" + img + """ />"""
@@ -40,11 +80,17 @@ def create_images(arr: list) -> str:
 
 
 def run(server_class=HTTPServer, handler_class=BaseHTTPRequestHandler):
-    server_address = ('', 8000)
+    """
+    Run server
+    :param server_class: - type of server
+    :param handler_class: - handler 'object' to handle user requests
+    :return: None
+    """
+    server_address = ('', 8000)  # we want to run on localhost
     print(f"server is running http://localhost:{8000}/")
     httpd = server_class(server_address, handler_class)
-    httpd.serve_forever()
+    httpd.serve_forever()  # start server
 
 
 if __name__ == '__main__':
-    run(handler_class=MyServer)
+    run(handler_class=MySyncServer)
--- utils/utils.py
+++ utils/utils.py
@@ -1,16 +1,36 @@
+"""Authors: Bc. Martin Smetanka
+University:  Slovak Technical University in Bratislava
+Faculty: Faculty of Electrical Engineering and Information Technology
+Semester: Spring/Summer
+Year: 2022
+License: MIT
+Assignment: https://uim.fei.stuba.sk/i-ppds/8-cvicenie-asynchronne-programovanie/"""
+
 import asyncio
 import time
-
 import matplotlib.pyplot as plt
 import requests
 
 
-def save_time(file: str, time: str):
+# Python v3.10
+
+def save_time(file: str, elapsed: str):
+    """
+    Save time in 'txt' file
+    :param file: str: - path to a file
+    :param elapsed: str: - time to write in file
+    :return:
+    """
     with open(file, 'a') as FILE:
-        FILE.write(time + '\n')
+        FILE.write(elapsed + '\n')
 
 
-def open_file(file: str):
+def open_file(file: str) -> list:
+    """
+    Open file and return content as list
+    :param file: str: - path to a file
+    :return: list: - list of content
+    """
     result = []
     with open(file, 'r') as FILE:
         for line in FILE:
@@ -19,12 +39,23 @@ def open_file(file: str):
 
 
 def compare_times(file1: str, file2: str):
+    """
+    Print average time from data stored in two files
+    :param file1: str: - path to a file
+    :param file2: str: - path to a file
+    :return: None
+    """
     f1, f2 = open_file(file1), open_file(file2)
     print(f"{file1}: {sum(f1) / len(f1)}")
     print(f"{file2}: {sum(f2) / len(f2)}")
 
 
-def get_data(file: str):
+def get_data(file: str) -> tuple:
+    """
+    Parse data from 'result.txt'
+    :param file: str: - path to file
+    :return: tuple: - parsed data
+    """
     x = []
     y_sync, y_async = [], []
     with open(file, 'r') as FILE:
@@ -34,7 +65,6 @@ def get_data(file: str):
             elif '-' in line:
                 x.append(int(line.replace('-', '')))
             else:
-
                 if 'async_elapsed.txt' in line:
                     y_async.append(float(line.replace('async_elapsed.txt: ', '').replace('\n', '')))
                 else:
@@ -43,26 +73,39 @@ def get_data(file: str):
 
 
 async def req(url):
+    """
+    Create request to an url
+    :param url: str: - url in which request is send to
+    :return: None
+    """
     r = requests.get(url=url)
     r.close()
 
 
 async def main():
+    """
+    Send requests and measure respond time.
+    Requests are send to 'appAsync.py' and 'appSync.py' in synchronous and asynchronous manner
+    :return: None
+    """
+
     start = time.time()
     async_server = 'http://127.0.0.1:8008/'
-    corr = [req(async_server) for _ in range(50)]
-    await asyncio.gather(*corr)
+    corr = [req(async_server) for _ in range(50)]  # send 50 request on async server
+    await asyncio.gather(*corr)  # wait for response
     end = time.time()
-    print(f'50 async calls --> async server {end-start} ')
+
+    print(f'50 async calls --> async server {end - start} ')
 
     start = time.time()
     sync_server = 'http://127.0.0.1:8000/'
-    corr = [req(sync_server) for _ in range(50)]
-    await asyncio.gather(*corr)
+    corr = [req(sync_server) for _ in range(50)]  # send 50 request on sync server
+    await asyncio.gather(*corr)  # wait for response
     end = time.time()
 
     print(f'50 async calls --> sync server {end - start} ')
 
+    # Send 50 request in sync order
     start = time.time()
     for _ in range(50):
         r = requests.get(url=async_server)
@@ -76,15 +119,16 @@ async def main():
         r = requests.get(url=sync_server)
         r.close()
     end = time.time()
+
     print(f'50 sync calls --> sync server {end - start} ')
 
+
 if __name__ == '__main__':
-    # asyncio.run(main())
-    # compare_times('async_elapsed.txt', 'sync_elapsed.txt')
+    asyncio.run(main())
+    compare_times('async_elapsed.txt', 'sync_elapsed.txt')
+
+    # Show performance
     x, y_sync, y_async = get_data('results.txt')
-    print(x)
-    print(y_sync)
-    # plot lines
     plt.plot(x, y_sync, label="sync")
     plt.plot(x, y_async, label="async")
     plt.legend()
